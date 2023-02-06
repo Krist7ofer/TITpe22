684bb657963c642d43bc94fed3c4fb356faa3afa
Krist7ofer <118337134+Krist7ofer@users.noreply.github.com>
2023-02-06 13:02:22 +0200

    Nüüd on palju sõnumeid

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Tere Maailm, veel üks muudatus");
+Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus");
+Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus");
+Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus");
+Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus");
+Console.WriteLine("Tere Maailm, veel üks muudatus"); Console.WriteLine("Tere Maailm, veel üks muudatus");
